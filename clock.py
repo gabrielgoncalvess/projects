c3ff78ed7c980e1c6fe77840a85933161c96c3da
gabrielgoncalvess <72632940+gabrielgoncalvess@users.noreply.github.com>
2021-02-20 12:18:17 -0300

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 12:12:40 2021
+
+@author: gabre
+"""
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+    
+label = Label(root, font=("ds-digital",80), background = "black", foreground = "cyan")
+label.pack(anchor='center')
+time()
+
+mainloop()
