3d861683ba65f8cf6986c76bddcf1687dd0922ec
gabrielgoncalvess <72632940+gabrielgoncalvess@users.noreply.github.com>
2021-02-23 14:13:23 -0300

    Update twilio_API.py

--- twilio_API.py
+++ twilio_API.py
@@ -8,8 +8,8 @@ Created on Mon Feb 22 20:50:58 2021
 from twilio.rest import Client
 
 client = Client(
-    "ACcf85dc43741e9a83b53992b2a3986ab9", 
-    "b3e8a684d15301e806476a6ecea1f082")
+    "API_ID", 
+    "AUTH_CODE")
 
 for msg in client.messages.list():
     print(f"Deleting {msg.body}")
@@ -20,8 +20,8 @@ for msg in client.messages.list():
 
 '''
 msg = client.messages.create(
-    to="+5521987327066",
-    from_="+18577073936",
+    to="number_to",
+    from_="number_from",
     body="Hello from Python"
     )
 
