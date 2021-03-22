92468bcc19e70c3ef2a53f85ef0313d44f5511bf
gabrielgoncalvess <72632940+gabrielgoncalvess@users.noreply.github.com>
2021-03-22 17:33:32 -0300

    Use sql with python

---
+++ sql.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 15:52:48 2021
+
+@author: gabre
+"""
+
+import pandas as pd
+import sqlalchemy
+
+# mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>
+
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://<user>:<password>@<host>[:<port>]/<dbname>')
+
+#df = pd.read_sql_table('products', engine, columns=['product_id', 'quantity_in_stock', 'unit_price'])
+
+
+#df = pd.read_sql_query("SELECT * FROM products", engine, index_col='product_id')
+
+query = """
+SELECT 
+    productCode, 
+    productName, 
+    textDescription
+FROM
+    products t1
+INNER JOIN productlines t2 
+    ON t1.productline = t2.productline;
+"""
+
+
+df2 = pd.read_excel(r"Consumo_Cerveja.xlsx")
+df2 = df2.drop(columns=["Unnamed: 7", "Unnamed: 8", "Unnamed: 9", "Unnamed: 10", "Unnamed: 11", "Unnamed: 12", "Unnamed: 13"])
+
+# salvar no mysql
+df2.to_sql(name = 'consumo_cerveja', con = engine, index = False) # if_exists = 'append' or if_exists = 'replace'
